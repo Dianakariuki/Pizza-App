4bade51c6e23b66c15d5fcf4512c97c735e7f258
Diana Kariuki <dianakariuki842@gmail.com>
2022-03-19 12:37:39 +0300

    getting the delivery price

--- index.js
+++ index.js
@@ -8,16 +8,18 @@ document.getElementById("placeOrder").addEventListener("click", placeOrder);
  */
  function placeOrder() {
     // get form values
-    var flavPizzas = document.getElementById("flavor").value;
+    var typePizza = document.getElementById("flavor").value;
     var sizePizza = document.getElementById("size").value;
-    var sizePizza = document.getElementById("size").value;
+    var crustPizza= document.getElementById("crust").value;
+    var ptoppings= document.getElementById("toppings").value;
 
     var deliveryCity = document.getElementById("deliveryCity").value;
 
-    
+
   // get the pizza price
-  var orderPrice = calculatePrice(numPizzas, typePizza);
+  var orderPrice = calculatePrice(typePizza,sizePizza,crustPizza,ptoppings);
 
-
+ // get the delivery price
+ var deliveryPrice = calculateDelivery(orderPrice, deliveryCity);
 
  }

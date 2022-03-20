c832a76c56c3b639b181c189a14dcf966fcf5cce
Diana Kariuki <dianakariuki842@gmail.com>
2022-03-20 16:20:51 +0300

    created gh-pages branch

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
+$(document).ready(function() {
+    $("#form2").submit(function(event) {
+        event.preventDefault();
+        var size = $("input#sizes option:selected").val();
+        var crust = $("input#crusts option:selected").val();
+        var toppings = $("input#toppings option:selected").val();
+        var numPizza = $("#numPizzas").val();
+        var deliveryCity = $("input#delivery option:selected").val();
 
+        var newGetPizza = new Getpizza(size, crust, toppings, numPizza, deliveryCity);
+        var totalPrice = parseInt((newGetPizza.getSizePrice() + newGetPizza.getCrustPrice() + newGetPizza.gettoppingsPrice() + newGetPizza.getdeliveryCity())) * numPizza;
+        alert("Your total is" + totalPrice)
 
+    });
+
+});
 //constructors and propotypes
 
 class Getpizza {
@@ -13,62 +27,58 @@ class Getpizza {
     }
 };
 
-Getpizza.prototype.getSizePrice = function () {
+Getpizza.prototype.getSizePrice = function() {
     if (this.size == "small") {
-        return 500;
+
+        console.log(500);
     } else if (this.size == "medium") {
-        return 800;
-    } else if (this.size =="large") {
-        return 1200;
-    }else { return "Nothing selected,choose an option"
-}};
-  
-Getpizza.prototype.gettoppingsPrice = function () {
-    if (this.toppings =="Mushroom"||"Extra-olive"||"Sausage"||"Bacon") {
-        return 50;
-     }else{ return "Nothing selected, Chose an option"
-}};
-Getpizza.prototype.getCrustPrice = function () {
+
+        console.log(800);
+    } else if (this.size == "large") {
+
+        console.log(1200);
+    } else {
+
+        console.log("choose an option");
+    }
+};
+
+Getpizza.prototype.gettoppingsPrice = function() {
+    if (this.toppings == "Mushroom" || "Extra-olive" || "Sausage" || "Bacon") {
+
+        console.log(50);
+    } else {
+
+        console.log("Chose an option")
+    }
+};
+Getpizza.prototype.getCrustPrice = function() {
     if (this.crust == "Crispy") {
-        return 100;
+
+        console.log(100);
     } else if (this.crust == "Stuffed") {
-        return 150;
-    } else if (this.crust =="Gluten-free") {
-        return 200;
-  
-    }else{ return "Nothing selected,Choose an option"
-}};
-Getpizza.prototype.getdeliveryCity = function () {
+
+        console.log(150);
+    } else if (this.crust == "Gluten-free") {
+        console.log(200);
+
+    } else {
+
+        console.log("Choose an option");
+    }
+};
+Getpizza.prototype.getdeliveryCity = function() {
     if (this.deliveryCity == "Waiyaki-way") {
-        return 100;
+        console.log(100);
     } else if (this.deliveryCity == "Thika-road") {
-        return 150;
-    } else if (this.deliveryCity =="Eastern-Bypass") {
-        return 200;
-  
-    }else if(this.deliveryCity== "Western-Bypass"){
-        return 300
-    }};
-    
-    
-    
 
-//user interface
+        console.log(150);
+    } else if (this.deliveryCity == "Eastern-Bypass") {
 
-$(document).ready(function () {
-    $("#form2").submit(function (event) {
-        event.preventDefault();
-        var size = $("input#sizes option:selected").val();
-        var crust = $("input#crusts option:selected").val();
-        var toppings = $("input#toppings option:selected").val();
-        var numPizza = parseInt($("#numPizzas").val());
-        var deliveryCity = $("input#delivery option:selected").val();
+        console.log(200);
 
-        var newGetPizza = new Getpizza(size, crust, toppings, numPizza,deliveryCity);
-        var totalPrice = (newGetPizza.getSizePrice()+newGetPizza.getCrustPrice()+newGetPizza.gettoppingsPrice()+getdeliveryCity)*numPizza;
+    } else(this.deliveryCity == "Western-Bypass"); {
 
-        alert("Your order is "+numPizza+ " " +size+ " pizzas, with a " + crust +" crust , "+toppings + " toppings!" +deliveryCity+ "delivery");
-        alert("The total cost is  " + totalPrice );
-    });
-
-});
+        console.log(300);
+    }
+};

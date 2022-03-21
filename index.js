d14c331396a5e2e74988b76831e5d88721c24159
Diana Kariuki <dianakariuki842@gmail.com>
2022-03-21 21:46:01 +0300

    fixing bugs

--- index.js
+++ index.js
@@ -1,83 +1,93 @@
 //constructors and propotypes
 
-class Getpizza {
-    constructor(name, size, crust, toppings, deliveryCity, total) {
-        this.name = name;
+class getPizza {
+    constructor(size, crust, toppings, numPizza, deliveryCity) {
+
         this.size = size;
         this.crust = crust;
         this.toppings = toppings;
+        this.numPizza = numPizza;
         this.deliveryCity = deliveryCity;
-        this.total = total;
     }
 };
 
-Getpizza.prototype.getSizePrice = function() {
-    if (this.size == "small") {
+getPizza.prototype.getSizePrice = function() {
 
-        console.log(500);
+
+    if (this.size == "large") {
+
+        return 1200;
     } else if (this.size == "medium") {
 
-        console.log(800);
-    } else if (this.size == "large") {
+        return 800;
+    } else
+    if (this.size == "small") {
 
-        console.log(1200);
+        return 500;
     } else {
 
         console.log("choose an option");
     }
 };
 
-Getpizza.prototype.gettoppingsPrice = function() {
-    if (this.toppings == "Mushroom" || "Extra-olive" || "Sausage" || "Bacon") {
+getPizza.prototype.getToppingsPrice = function() {
+    if (this.toppings == "Mushroom" || this.toppings == "Extra-olive" || this.toppings == "Sausage" || this.toppings == "Bacon") {
 
-        console.log(50);
+        return 50;
     } else {
 
         console.log("Choose an option")
     }
 };
-Getpizza.prototype.getCrustPrice = function() {
+getPizza.prototype.getCrustPrice = function() {
     if (this.crust == "Crispy") {
 
-        console.log(100);
+        return 100;
     } else if (this.crust == "Stuffed") {
 
-        console.log(150);
-    } else if (this.crust == "Gluten-free") {
-        console.log(200);
+        return 150;
+    } else
+    if (this.crust == "Gluten-free") {
+        return 200;
 
     } else {
 
         console.log("Choose an option");
     }
 };
-Getpizza.prototype.getdeliveryCity = function() {
+getPizza.prototype.getDeliveryCity = function() {
     if (this.deliveryCity == "Waiyaki-way") {
-        console.log(100);
+        return 100;
     } else if (this.deliveryCity == "Thika-road") {
 
-        console.log(150);
-    } else if (this.deliveryCity == "Eastern-Bypass") {
+        return 150;
+    } else
+    if (this.deliveryCity == "Eastern-Bypass") {
 
-        console.log(200);
+        return 200;
 
-    } else(this.deliveryCity == "Western-Bypass"); {
+    } else if (this.deliveryCity == "Western-Bypass"); {
 
-        console.log(300);
+        return 300;
     }
 };
 $(document).ready(function() {
     $("#form2").submit(function(event) {
         event.preventDefault();
-        var size = $("input#sizes option:selected").val();
-        var crust = $("input#crusts option:selected").val();
-        var toppings = $("input#toppings option:selected").val();
-        var numPizza = $("#numPizzas").val();
-        var deliveryCity = $("input#delivery option:selected").val();
+        var size = $("select#size option:selected").val();
+        var crust = $("select#crusts option:selected").val();
+        var toppings = $("select#toppings option:selected").val();
+        var numPizza = $("#number").val();
+        var deliveryCity = $("select#delivery option:selected").val();
+        console.log(numPizza)
 
-        var newGetPizza = new Getpizza(size, crust, toppings, numPizza, deliveryCity);
-        var totalPrice = parseInt((newGetPizza.getSizePrice() + newGetPizza.getCrustPrice() + newGetPizza.gettoppingsPrice() + newGetPizza.getdeliveryCity())) * numPizza;
-        alert("Your total is" + totalPrice)
+        var newGetPizza = new getPizza(size, crust, toppings, numPizza, deliveryCity);
+        var totalPrice = (newGetPizza.getSizePrice() + newGetPizza.getCrustPrice() + newGetPizza.getToppingsPrice() + newGetPizza.getDeliveryCity()) * numPizza;
+
+        alert("Thank you for your order, Your total is" + " " +
+            "ksh" +
+            " " + totalPrice);
+            prompt("Please input your contact details");
 
     });
 
